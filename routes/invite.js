a8d71ce32f0d01d4b329f68f795571878cfa0f18
Robyn <robynover@gmail.com>
2017-06-08 16:56:12 -0400

    remove log in requirement from invite code pages

--- routes/invite.js
+++ routes/invite.js
@@ -9,102 +9,112 @@ module.exports = function(express){
 	var InvitationAllotment = require('../models/mongoose/invitationAllotment.js');
 
 	// === require users to be logged in for this section=== //
-	router.all('/*',function(req,res,next){
+	/*router.all('/*',function(req,res,next){
 		if (req.user){
 			next();
 		} else {
 			res.status(401);
 			res.render('generic',{msg:'You must be logged in to view this page'});
 		}	
-	});
+	});*/
 
 	router.get('/',(req,res)=>{
-		InvitationAllotment.findByUsername(req.user.username)
-			.then( (doc) =>{
-				var context = {};
-				context.username = req.user.username;
-				context.is_admin = req.user.is_admin;
-				context.loggedin = true;
-				if (doc){
-					context.numLeft = doc.left;
-				} else {
-					context.numLeft = 0;
-				}
-				
-				if (context.numLeft < 0){
-					context.numLeft = 0;
-				}
+		if (req.user){
+			InvitationAllotment.findByUsername(req.user.username)
+				.then( (doc) =>{
+					var context = {};
+					context.username = req.user.username;
+					context.is_admin = req.user.is_admin;
+					context.loggedin = true;
+					if (doc){
+						context.numLeft = doc.left;
+					} else {
+						context.numLeft = 0;
+					}
+					
+					if (context.numLeft < 0){
+						context.numLeft = 0;
+					}
 
-				res.render('inviteform',context);
-			})
-			.catch(err=>{
-				//console.log(err);
-				res.render('generic',{msg:err});
-			});
+					res.render('inviteform',context);
+				})
+				.catch(err=>{
+					//console.log(err);
+					res.render('generic',{msg:err});
+				});
+		} else {
+			res.render('generic',{msg:'You must be logged in to view this page'});
+		}
+		
 	});
 
 	router.post('/',(req,res)=>{
-		var context = {};
-		context.username = req.user.username;
-		context.is_admin = req.user.is_admin;
-		context.loggedin = true;
+		if (req.user){
+			var context = {};
+			context.username = req.user.username;
+			context.is_admin = req.user.is_admin;
+			context.loggedin = true;
 
-		// how many invitations does the user have available?
-		InvitationAllotment.findByUsername(req.user.username)
-			.then( (doc) =>{
-				var numLeft = doc.left;
+			// how many invitations does the user have available?
+			InvitationAllotment.findByUsername(req.user.username)
+				.then( (doc) =>{
+					var numLeft = doc.left;
 
-				// get emails from form, trim and validate
-				var emails = req.body.emails.split(',').map(function(x) {
-				   return x.trim();
-				}).filter(function(v){
-					return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
-				});
-				
-				if (emails.length <= numLeft){
+					// get emails from form, trim and validate
+					var emails = req.body.emails.split(',').map(function(x) {
+					   return x.trim();
+					}).filter(function(v){
+						return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
+					});
 					
-					var promises = [];
+					if (emails.length <= numLeft){
+						
+						var promises = [];
 
-					for (var i in emails){
-						var obj = {};
-						obj.invitee_email = emails[i];
-						obj.inviter_id = req.user.id;
-						obj.inviter_username = req.user.username;
-						obj.inviter_firstname = req.user.firstname;
+						for (var i in emails){
+							var obj = {};
+							obj.invitee_email = emails[i];
+							obj.inviter_id = req.user.id;
+							obj.inviter_username = req.user.username;
+							obj.inviter_firstname = req.user.firstname;
 
-						var invitation = new Invitation(obj);
-						invitation.generateCode();
-						invitation.save()
-							.then(inv=>{
-								// save to invitation allotment record
-								promises.push(doc.addInvite(inv._id));	
-							})
-							.catch(err=>{
-								console.log(err.message);
-							});
-							
-					}
-					return Promise.all(promises).then( ()=>{
-						var word = 'invitations';
-						if (emails.length == 1){ word = 'invitation';}
-						context.result = emails.length + " " + word + " sent";
-						context.numLeft = numLeft - emails.length;
-						if (context.numLeft < 0){
-							context.numLeft = 0;
+							var invitation = new Invitation(obj);
+							invitation.generateCode();
+							invitation.save()
+								.then(inv=>{
+									// save to invitation allotment record
+									promises.push(doc.addInvite(inv._id));	
+								})
+								.catch(err=>{
+									console.log(err.message);
+								});
+								
 						}
-						console.log(doc.left);
-					    res.render('inviteform',context);	
-					})
-						.catch(err=>{
-							context.result = err;
-							res.render('inviteform',context);
-							console.log(err);
-						});
-				} else {
-					context.result = "You do not have enough invites to invite "+emails.length+ " people";
-					res.render('inviteform',context);
-				}
-			});
+						return Promise.all(promises).then( ()=>{
+							var word = 'invitations';
+							if (emails.length == 1){ word = 'invitation';}
+							context.result = emails.length + " " + word + " sent";
+							context.numLeft = numLeft - emails.length;
+							if (context.numLeft < 0){
+								context.numLeft = 0;
+							}
+							console.log(doc.left);
+						    res.render('inviteform',context);	
+						})
+							.catch(err=>{
+								context.result = err;
+								res.render('inviteform',context);
+								console.log(err);
+							});
+					} else {
+						context.result = "You do not have enough invites to invite "+emails.length+ " people";
+						res.render('inviteform',context);
+					}
+				});
+		} else {
+			res.render('generic',{msg:'You must be logged in to view this page'});
+		}
+		
 
 	});
 
